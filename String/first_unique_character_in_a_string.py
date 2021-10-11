6a3c4888a98da2a4f50b576a7aa500314c605b30
duong-le <leduong26298@gmail.com>
2021-10-12 00:24:40 +0700

    feat: Add solution for `First Unique Character in a String`

---
+++ String/first_unique_character_in_a_string.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/first-unique-character-in-a-string/
+# O(N) T - O(1) S
+
+def first_unique_character_in_a_string(string: str) -> int:
+    character_frequencies = {}
+
+    for character in string:
+        character_frequencies[character] = character_frequencies.get(character, 0) + 1
+
+    for index, character in enumerate(string):
+        if character_frequencies[character] == 1:
+            return index
+
+    return -1
