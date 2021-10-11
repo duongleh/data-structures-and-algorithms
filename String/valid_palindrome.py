b4857f283029f06dd342d2f84a567db8bbdca85a
duong-le <leduong26298@gmail.com>
2021-10-11 23:35:57 +0700

    feat: Add solution for `Valid Palindrome`

---
+++ String/valid_palindrome.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/valid-palindrome/
+# O(N) T - O(1) S
+
+def is_palindrome(string: str) -> bool:
+    index_from_start = 0
+    index_from_end = len(string) - 1
+
+    while index_from_start < index_from_end:
+        if not string[index_from_start].isalnum():
+            index_from_start += 1
+            continue
+
+        if not string[index_from_end].isalnum():
+            index_from_end -= 1
+            continue
+
+        if string[index_from_start].lower() != string[index_from_end].lower():
+            return False
+
+        index_from_start += 1
+        index_from_end -= 1
+
+    return True
