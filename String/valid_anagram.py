d326e081ec2b8cdb127656cbc6410b44df5620f5
duong-le <leduong26298@gmail.com>
2021-10-11 23:45:55 +0700

    feat: Add solution for `Valid Anagram`

---
+++ String/valid_anagram.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/valid-anagram/
+# O(N) TS
+
+def is_anagram(first_string: str, second_string: str) -> bool:
+    if len(first_string) != len(second_string):
+        return False
+
+    table = {}
+    for character in first_string:
+        table[character] = table.get(character, 0) + 1
+
+    for character in second_string:
+        if character not in table:
+            return False
+
+        table[character] -= 1
+        if table[character] == 0:
+            table.pop(character)
+
+    return table == {}
