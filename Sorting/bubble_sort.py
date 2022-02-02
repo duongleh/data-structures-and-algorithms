136f8921ed6df5029033e2d405eeefee8d4bc59c
duong-le <leduong26298@gmail.com>
2022-02-02 22:03:14 +0700

    feat: Add solution for `Bubble Sort`

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,25 @@
+# https://www.algoexpert.io/questions/Bubble%20Sort
+# O(N^2) T - O(1) S
+
+def bubble_sort(numbers: list[int]):
+    counter = 0
+    is_sorted = False
+    while not is_sorted:
+        is_sorted = True
+
+        # After each iteration, the largest element among the unsorted elements is placed at the end
+        # Therefore, we don't have to go all the way to the end of the list
+        for index in range(len(numbers) - counter - 1):
+            if numbers[index] <= numbers[index + 1]:
+                continue
+
+            _swap(numbers, index, index + 1)
+            is_sorted = False
+
+        counter += 1
+
+    return numbers
+
+
+def _swap(numbers: list[int], left_index: int, right_index: int):
+    numbers[left_index], numbers[right_index] = numbers[right_index], numbers[left_index]
