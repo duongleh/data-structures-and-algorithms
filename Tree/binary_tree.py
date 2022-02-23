41edc37bc0fb25b2de4adeded85a873b0ee61e9e
duong-le <leduong26298@gmail.com>
2022-02-23 23:39:22 +0700

    feat: Add implementation for `Binary Tree`

---
+++ Tree/binary_tree.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class TreeNode:
+    value: int = 0
+    left_child: TreeNode = None
+    right_child: TreeNode = None
+
+    def is_leaf(self) -> bool:
+        return self.left_child == None and self.right_child == None
