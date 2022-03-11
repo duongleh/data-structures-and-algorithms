c964de2ea70ecf8c8be68aa1ce062bdab80206b2
duong-le <leduong26298@gmail.com>
2022-03-12 00:14:44 +0700

    feat: Add solution for `Lowest Common Ancestor of a Binary Tree`

---
+++ Tree/lowest_common_ancestor_of_binary_tree.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree
+# O(N) T - O(N) S
+
+from Tree.binary_tree import TreeNode
+
+
+def lowest_common_ancestor_of_binary_tree(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    if not root:
+        return None
+
+    if root in [p, q]:
+        return root
+
+    left_common_ancestor = self.lowestCommonAncestor(root.left_child, p, q)
+    right_common_ancestor = self.lowestCommonAncestor(root.right_child, p, q)
+
+    if left_common_ancestor and right_common_ancestor:
+        return root
+
+    return left_common_ancestor or right_common_ancestor
