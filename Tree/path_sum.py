851b92ec445835c90e68137fa1f7e8b0b47fe7e3
duong-le <leduong26298@gmail.com>
2022-03-11 15:57:45 +0700

    feat: Add solution for `Path Sum`

---
+++ Tree/path_sum.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/path-sum
+# O(N) T - O(N) S
+
+
+from typing import Optional
+
+from Tree.binary_tree import TreeNode
+
+
+def path_sum(root: Optional[TreeNode], target_sum: int) -> bool:
+    if not root:
+        return False
+
+    target_sum -= root.val
+
+    if not root.left and not root.right:
+        return target_sum == 0
+
+    return path_sum(root.left, target_sum) or path_sum(root.right, target_sum)
