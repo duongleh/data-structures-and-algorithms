24847ecf246b6d88f645fda91a486cc3f2cdffeb
duong-le <leduong26298@gmail.com>
2022-03-11 16:47:31 +0700

    feat: Add solution for `Construct Binary Tree from Inorder and Postorder Traversal`

--- Tree/construct_binary_tree_from_traversal.py
+++ Tree/construct_binary_tree_from_traversal.py
@@ -17,3 +17,19 @@ def construct_from_preorder_and_inorder(preorder: list[int], inorder: list[int])
     root_node.right = construct_from_preorder_and_inorder(preorder, inorder[root_index + 1 :])
 
     return root_node
+
+
+# https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal
+# O(N) T - O(N) S
+def construct_from_inorder_and_postorder(inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
+    if not inorder:
+        return None
+
+    root_node = TreeNode(postorder.pop())
+
+    root_index = inorder.index(root_node.value)
+
+    root_node.right = construct_from_inorder_and_postorder(inorder[root_index + 1 :], postorder)
+    root_node.left = construct_from_inorder_and_postorder(inorder[0:root_index], postorder)
+
+    return root_node
