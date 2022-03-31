aa6f8972ed63b1dae5f95a398dfb30bbd091485f
duong-le <leduong26298@gmail.com>
2022-03-31 09:39:17 +0700

    feat: Add solution for `All Paths From Source to Target`

---
+++ Graph/all_paths_from_source_to_target.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/all-paths-from-source-to-target
+# O(2^V*V) T | O(V+E) S
+
+from graph import AdjacencyListGraph
+
+
+def all_paths_from_source_to_target(graph: AdjacencyListGraph) -> list[list[int]]:
+    return dfs(graph, 0, graph.vertex_count() - 1, [], [])
+
+
+def dfs(
+    graph: AdjacencyListGraph,
+    source: int,
+    target: int,
+    path: list[int],
+    paths: list[list[int]],
+):
+    path.append(source)
+
+    if source == target:
+        paths.append(path)
+        return
+
+    for adjacency_vertex in graph.get_adjacent_vertices(source):
+        dfs(graph, adjacency_vertex, target, [*path], paths)
+
+    return paths
