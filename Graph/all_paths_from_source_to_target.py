2bc3f3fa8782685feaec409d3c5c18bb5c4925ab
duong-le <leduong26298@gmail.com>
2022-04-17 22:58:56 +0700

    refactor: Move helper function to inner scope

--- Graph/all_paths_from_source_to_target.py
+++ Graph/all_paths_from_source_to_target.py
@@ -5,23 +5,22 @@ from graph import AdjacencyListGraph
 
 
 def all_paths_from_source_to_target(graph: AdjacencyListGraph) -> list[list[int]]:
+    def dfs(
+        graph: AdjacencyListGraph,
+        source: int,
+        target: int,
+        path: list[int],
+        paths: list[list[int]],
+    ):
+        path.append(source)
+
+        if source == target:
+            paths.append(path)
+            return
+
+        for adjacent_vertex in graph.get_adjacent_vertices(source):
+            dfs(graph, adjacent_vertex, target, [*path], paths)
+
+        return paths
+
     return dfs(graph, 0, graph.vertex_count() - 1, [], [])
-
-
-def dfs(
-    graph: AdjacencyListGraph,
-    source: int,
-    target: int,
-    path: list[int],
-    paths: list[list[int]],
-):
-    path.append(source)
-
-    if source == target:
-        paths.append(path)
-        return
-
-    for adjacent_vertex in graph.get_adjacent_vertices(source):
-        dfs(graph, adjacent_vertex, target, [*path], paths)
-
-    return paths
