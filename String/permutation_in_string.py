730c99a5d6299abae3a8b08435fae0e8d8fcf8a4
duong-le <leduong26298@gmail.com>
2022-06-26 23:17:22 +0700

    feat: Add a simpler solution for `Permutation in String`

--- String/permutation_in_string.py
+++ String/permutation_in_string.py
@@ -8,16 +8,27 @@ def permutation_in_string(pattern: str, string: str) -> bool:
         character_frequency[character] = character_frequency.get(character, 0) + 1
 
     start = 0
-    for character in string:
+    for end, character in enumerate(string):
+        # Add new character into window
         character_frequency[character] = character_frequency.get(character, 0) - 1
-        while character_frequency[character] < 0:
-            character_frequency[string[start]] = character_frequency.get(string[start], 0) + 1
-            start += 1
-
         if character_frequency[character] == 0:
             character_frequency.pop(character)
 
         if not character_frequency:
             return True
+
+        if end - start + 1 <= len(pattern):
+            continue
+
+        # Remove out of range character from window,
+        # keeping the length of window equals to the length of pattern
+        character_frequency[string[start]] = character_frequency.get(string[start], 0) + 1
+        if character_frequency[string[start]] == 0:
+            character_frequency.pop(string[start])
+
+        if not character_frequency:
+            return True
+
+        start += 1
 
     return False
