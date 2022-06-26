6e58fa2ee8467ad09bd348ab5d9e05c50549b5ec
duong-le <leduong26298@gmail.com>
2022-06-27 00:05:45 +0700

    chore: Slight improvement for readability

--- Array/max_consecutive_ones_iii.py
+++ Array/max_consecutive_ones_iii.py
@@ -3,18 +3,19 @@
 
 
 def find_max_consecutive_ones(nums: list[int], k: int) -> int:
-    consecutive_ones_count = 0
+    consecutive_zero_count = 0
     start = 0
     max_length = 0
     for end, num in enumerate(nums):
-        if num == 1:
-            consecutive_ones_count += 1
+        if num == 0:
+            consecutive_zero_count += 1
 
         # If the number of 0 are more then k, it is the time to shrink the window
-        while end - start + 1 - consecutive_ones_count > k:
-            if nums[start] == 1:
-                consecutive_ones_count -= 1
+        while consecutive_zero_count > k:
+            if nums[start] == 0:
+                consecutive_zero_count -= 1
             start += 1
 
         max_length = max(max_length, end - start + 1)
+
     return max_length
